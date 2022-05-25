087ec65881bcb9e06b1d4d447a5635e1e3bfaede
SaileshAlla <saileshalla97@gmail.com>
2022-05-25 00:54:23 -0400

    first commit

---
+++ 01_basics/hello.py
@@ -0,0 +1,9 @@
+# print("Hello World")
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+desc = "I love chitti"
+
+print("Hi, my name is "+name+" and I am "+age+" years old and "+desc)
---
+++ 01_basics/qu.py
@@ -0,0 +1,64 @@
+# import math as m
+'''
+a = m.log(100,5)
+print(a)
+
+
+x = "'Bombay'"
+y = "'Hyderabad'"
+z = x +' '+'to'+' '+ y
+print(z)
+print(z.split('a'))
+
+txt = "welcome to the jungle"
+print(txt.split("e"))
+
+
+
+import keyword
+print(keyword.kwlist)
+
+'''
+'''
+d = {'actor':'yash','animal':['cat','dog','fish'],'name':'lalith'}
+
+print(d)
+for val in d:
+    print(val)
+
+
+d['movie'] = 'speed'
+d.items()
+for a,b in d.items():
+    print(a,":",b)
+
+'''
+'''
+l = [1,2,3]
+print(type(l))
+l[2]=4
+print(l)
+
+t = (1,2,3)
+print(type(t))
+
+'''
+'''
+x=[2,3,4,5,6,8,9,-10,0,33,44,12]
+sqrs = []
+for sqr in x:
+    sqrs.append(sqr**2)
+print(x)
+print(sqrs)
+'''
+'''
+
+s = [1,2,3,70,-10,0,99,"a","b","c"]
+s[1:4]=['sailesh']
+s.extend(['sam ji','chiiti'])
+print(s)
+print(s.index('sam ji'))
+s[8]='sam ji loves chiiti'
+print(s)
+'''
+
