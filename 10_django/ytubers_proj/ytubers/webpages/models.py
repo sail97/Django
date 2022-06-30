cefe27c7a0a6477e623be3eb854699b44785291a
sail97 <saileshalla97@gmail.com>
2022-06-30 15:50:19 -0400

    Ytubers2

--- 10_django/ytubers_proj/ytubers/webpages/models.py
+++ 10_django/ytubers_proj/ytubers/webpages/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Slider(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    button_text = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to='slider/%Y/%m')
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
