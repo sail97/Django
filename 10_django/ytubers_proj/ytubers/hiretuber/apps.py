a816a5891dec92c878757abd2013b4dfb1a14534
sail97 <saileshalla97@gmail.com>
2022-08-01 18:17:50 -0400

    done

--- 10_django/ytubers_proj/ytubers/hiretuber/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HiretuberConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hiretuber'
--- Newproj/projects/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def projects(request):
-    return render(request,'projects/projects.html')
-
-def single_project(request,pk):
-    return render(request,'projects/single_project.html')
